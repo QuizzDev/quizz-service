6a3df3f72e7b6157c304ce497c7df82f1b4f9444
jschaedl <schaedlich.jan@gmail.com>
2015-08-18 13:22:17 +0200

    Added admin controllers for question and answer

---
+++ src/Quizz/Bundle/AdminBundle/Admin/AnswerAdmin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Quizz\Bundle\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class AnswerAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('target')
+            ->add('importance')
+            ->add('created')
+            ->add('updated')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('target')
+            ->add('importance')
+            ->add('created')
+            ->add('updated')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('target')
+            ->add('importance')
+            ->add('created')
+            ->add('updated')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('target')
+            ->add('importance')
+            ->add('created')
+            ->add('updated')
+        ;
+    }
+}
