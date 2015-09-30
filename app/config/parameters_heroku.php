cbe0c5d7d486de44b913bafb670c35fec867cc44
jschaedl <schaedlich.jan@gmail.com>
2015-09-30 16:44:53 +0200

    Fixed database parameters

--- app/config/parameters_heroku.php
+++ app/config/parameters_heroku.php
@@ -2,13 +2,15 @@
 $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
 $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', $db['host']);
-$container->setParameter('database_port', '~');
-$container->setParameter('database_name', substr($url["path"], 1));
+$container->setParameter('database_port', $db['port']);
+$container->setParameter('database_name', substr($db["path"], 1));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
+$container->setParameter('secret', getenv('SECRET'));
+$container->setParameter('locale', 'en');
 
-file_put_contents("php://stderr", "database_host" . $db['host'] . "\n");
-file_put_contents("php://stderr", "database_port" . $db['port'] . "\n");
-file_put_contents("php://stderr", "database_name" . substr($url["path"], 1) . "\n");
-file_put_contents("php://stderr", "database_user" . $db['user'] . "\n");
-file_put_contents("php://stderr", "database_password" . $db['pass'] . "\n");
+// file_put_contents("php://stderr", "database_host" . $db['host'] . "\n");
+// file_put_contents("php://stderr", "database_port" . $db['port'] . "\n");
+// file_put_contents("php://stderr", "database_name" . substr($url["path"], 1) . "\n");
+// file_put_contents("php://stderr", "database_user" . $db['user'] . "\n");
+// file_put_contents("php://stderr", "database_password" . $db['pass'] . "\n");
