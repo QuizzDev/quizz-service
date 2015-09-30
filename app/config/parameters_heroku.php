b690ba47f52f20c21d612b7ebf6f8e83079fdc42
jschaedl <schaedlich.jan@gmail.com>
2015-09-30 16:25:06 +0200

    Fixed database parameters

--- app/config/parameters_heroku.php
+++ app/config/parameters_heroku.php
@@ -6,3 +6,9 @@ $container->setParameter('database_port', '~');
 $container->setParameter('database_name', substr($url["path"], 1));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
+
+file_put_contents("php://stderr", "database_host" . $db['host']);
+file_put_contents("php://stderr", "database_port" . $db['port']);
+file_put_contents("php://stderr", "database_name" . substr($url["path"], 1));
+file_put_contents("php://stderr", "database_user" . $db['user']);
+file_put_contents("php://stderr", "database_password" . $db['pass']);
