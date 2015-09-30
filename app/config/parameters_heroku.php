d584b428796d9f7c501239064491140391055987
jschaedl <schaedlich.jan@gmail.com>
2015-09-30 14:48:50 +0200

    Fixed database parameters for heroku

--- app/config/parameters_heroku.php
+++ app/config/parameters_heroku.php
@@ -1,15 +1,8 @@
 <?php
-$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
-
+$db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
 $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', $db['host']);
 $container->setParameter('database_port', $db['port']);
-$container->setParameter('database_name', substr($db["path"], 1));
+$container->setParameter('database_name', trim($db['name'], '/'));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
-$container->setParameter('secret', getenv('SECRET'));   // mighty-sierra-3926
-$container->setParameter('locale', 'en');
-$container->setParameter('mailer_transport', null);
-$container->setParameter('mailer_host', null);
-$container->setParameter('mailer_user', null);
-$container->setParameter('mailer_password', null);
